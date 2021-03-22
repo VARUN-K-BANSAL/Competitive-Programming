d25d2aa9111107c032d88fab7db2f74df1c6291c
Varun <varunbansal326@gmail.com>
2021-03-22 18:02:59 +0530

    LeetCode Questions

---
+++ LeetCode/Add Binary.c
@@ -0,0 +1,26 @@
+char * addBinary(char * a, char * b){
+    int i, j, k, c = 0;
+    char *res;
+    
+    i = strlen(a);
+    j = strlen(b);
+    k = i > j ? i+2 : j+2;
+
+    res = malloc(k * sizeof(char));
+    
+    i--;
+    j--;
+    res[--k] = '\0';
+    
+    while (--k >= 0) {
+        c += i >= 0 ? a[i--] - '0': 0;
+        c += j >= 0 ? b[j--] - '0': 0;
+        
+        res[k] = c % 2 + '0';
+        c /= 2;
+    }
+    
+    if (res[0] == '0') memmove(res, res + 1, strlen(res) * sizeof(char));
+                                // memmove res+1 to res
+    return res;
+}
