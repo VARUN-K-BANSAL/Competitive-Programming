b20c42268d5e91f4f24ad0bc3db0514cfc84d23e
Varun <varunbansal326@gmail.com>
2021-03-25 13:43:49 +0530

    updated

---
+++ LeetCode/20. Valid Parenthesis.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack <char> s1;
+        int i=0;
+        while(s[i]!='\0'){
+            
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{') s1.push(s[i]);
+            else{
+                if(s[i] == ')'){
+                    if(!s1.empty() && s1.top()=='(') s1.pop();
+                    else break;
+                }
+                if(s[i] == ']'){
+                    if(!s1.empty() && s1.top()=='[') s1.pop();
+                    else break;
+                }
+                if(s[i] == '}'){
+                    if(!s1.empty() && s1.top()=='{') s1.pop();
+                    else break;
+                }
+            }
+            i++;
+        }
+        if(i==s.length() && s1.empty()) return true;
+        else return false;
+    }
+};
